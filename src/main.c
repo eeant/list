5a86007366a677ef1dc1ece0d3f3b14d49348b73
eeant <eeant@outlook.com>
2016-01-01 21:21:20 +0800

    PrintLots
    打印L中那些有P指定位置的元素

--- src/main.c
+++ src/main.c
@@ -12,24 +12,56 @@ void PrintList(List L)
 	}
 }
 
+void PrintLots(List L, List P)
+{
+	int  count = 0;
+	Position Lpos,Ppos;
+	Lpos = First(L);
+	Ppos = First(P);
+	count = 1;
+	while(Lpos != NULL && Ppos != NULL)
+	{
+		if (Ppos->Element == count++)
+		{
+			printf("%d\n",Lpos->Element);
+			Ppos = Advance(Ppos);
+		}
+		Lpos = Advance(Lpos);
+	}
+}
+
 
 int main(int argc,char *argv[])
 {
 	List L = NULL;
 	Position insertP = NULL;
+	List P = NULL;
 	int input = -1;
 	L = MakeEmpty(L);
 	insertP = L;
 	do
 	{
-		printf("Enter the Element:");
+		printf("Enter the L Element:");
 		scanf("%d",&input);
 		
 		Insert(input,L,insertP);		
 	}while(input!=-1);
 	
 	PrintList(L);	
-
+	
+	P = MakeEmpty(P);
+	insertP = P;
+	do{
+		printf("Enter the P Element:");
+		scanf("%d",&input);
+		
+		Insert(input,P,insertP);
+	}while(input > 1);
+	
+	
+	PrintList(P);
+	PrintLots(L,P);
 	DeleteList(L);
+	DeleteList(P);
 	return 0;
 }
