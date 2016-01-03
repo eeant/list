fd92ba6563a53ca622537a97c3bb2c2ad9543ae6
eeant <eeant@outlook.com>
2016-01-03 12:58:25 +0800

    修正指针错误
    增加判断指针的有效性

--- src/list.c
+++ src/list.c
@@ -2,135 +2,236 @@
 #include "fatal.h"
 #include <stdlib.h>
 
-/*Place in the interface file*/
+/* 
+List MakeEmpty(List L)
+参数 
+L 要生成的空链表名 
+返回值 
+返回生成的空链表名 
+功能 
+生成空链表 
+*/
 
 List MakeEmpty(List L)
 {
-	if (L != NULL)	
+	if ( !IsEmpty(L) )	/*不是空链表,删除*/
 		DeleteList(L);
-	L = malloc(sizeof(struct Node));
+		
+	L = (List)malloc(sizeof(struct Node));
 	if (L == NULL)
 		FatalError("Out of memory");
 	L->Next = NULL;
+	
 	return L;
 }
-/*Return true if L is empty*/
+
+/* 
+int IsEmpty(List L)
+参数 
+L 要判定的链表名 
+返回值 
+若L为空返回1，否则返回0 
+功能 
+判断链表L是否为空 
+*/
 int IsEmpty(List L)
 {
-	return L->Next == NULL;
+	return (L==NULL || L->Next == NULL);
 }
 
-/*Return true if P is the last position in list L*/
-/*Parameter L is unused in this implemention*/
-/*Return Position of X in L; NULL if not found*/
+/* 
+int IsLast(Position P, List L)
+参数 
+P 要判定的位置 
+返回值 
+若P为为最后一个节点则返回1，否则返回0 
+功能 
+判断位置P的节点是否是链表最后一个节点 
+*/ 
 int IsLast(Position P, List L)
 {
+	
 	return P->Next == NULL;
 }
-Position Find(ElementType X,List L)
+/* 
+Position Find(ElementType E,List L)
+参数 
+E 要查找的数据项 
+L 要查找的链表 
+返回值 
+若E在L中存在则返回第一个匹配节点的位置，否则返回NULL 
+功能 
+判断位置P的节点是否是链表最后一个节点 
+*/
+Position Find(ElementType E,List L)
 {
 	Position P;
 	
 	P = L->Next;
-	while(P !=NULL && P->Element != X)
+	while( P !=NULL && P->Element != E )
 		P = P->Next;
 
 	return P;
 }
 
-/*Delete form a list*/
-/*Cell pointed to by P->Next is wiped out*/
-/*Aussume that the position is legal*/
-/*Assume use of a header node*/
-void Delete(ElementType X, List L)
+/* 
+void Delete(ElementType E, List L)
+参数 
+E 要删除的数据项 
+L 要删除节点所在的链表 
+返回值 
+无 
+功能 
+在链表L中删除查找到的第一个数据项为E的节点 
+*/ 
+void Delete(ElementType E, List L)
 {
-	Position P,TmpCell;
+	Position P,tmpCell;
 	
-	P = FindPrevious(X,L);
+	P = FindPrevious(E, L);
 	
-	if ( !IsLast(P, L)) /*iAssumption of header use*/
-	{		    /*X is found;delete it*/
-		TmpCell = P->Next;
-		P->Next = TmpCell->Next; /*bypass deleted cell*/
-		free(TmpCell);
+	if ( !IsLast(P, L)) /*找到*/
+	{		    		
+		tmpCell = P->Next;
+		P->Next = tmpCell->Next; 
+		free(tmpCell);
 	}
 }
-/*If X is nod found, then Next field of returned value is NULL*/
-/*Assumes a header*/
-Position FindPrevious(ElementType X, List L)
+/* 
+Position FindPrevious(ElementType E, List L)
+参数 
+E 要查找的数据项 
+L 要查找的链表 
+返回值 
+若E在L中存在则返回第一个匹配节点的前驱位置，否则返回NULL 
+功能 
+返回链表L中数据项为X的节点的前驱节点位置 
+*/ 
+Position FindPrevious(ElementType E, List L)
 {
 	Position P;
 	P =  L->Next;
-	while(P->Next != NULL && P->Next->Element != X)
+	while(P->Next != NULL && P->Next->Element != E)
 		P = P->Next;
 	return P;
 
 }
-/*Insett after legal position P*/
-/*Header implementatioon assumed*/
-/*Parameter L is unused in this implementtation*/
-List Insert(ElementType X,List L, Position P)
+/*
+List Insert(ElementType E,List L, Position P) 
+参数 
+E 要插入的数据项 
+L 要插入的链表 
+返回值 
+无 
+功能 
+在链表L中P位置之后插入数据项为E的新节点 
+*/
+List Insert(ElementType E,List L, Position P)
 {
-	Position TmpCell;
-	TmpCell = malloc(sizeof(struct Node));
-	if (TmpCell == NULL)
+	Position tmpCell;
+	
+	tmpCell = malloc(sizeof(struct Node));
+	if (tmpCell == NULL)
 		FatalError("Out of space!!!");
 	
-	TmpCell->Element = X;
+	tmpCell->Element = E;
 	
 	if (P == NULL)
 	{
-		TmpCell->Next = L->Next;
-		L->Next=TmpCell;
+		tmpCell->Next = L->Next;
+		L->Next=tmpCell;
 	}
 	else
 	{
-		TmpCell->Next = P->Next;
-		P->Next = TmpCell;
+		tmpCell->Next = P->Next;
+		P->Next = tmpCell;
 	}
 
 
 	return L;
 }
-
+/* 
+void DeleteList(List L) 
+参数 
+L 要删除节点的链表 
+返回值 
+无 
+功能 
+删除链表L中所有节点 
+*/ 
 void DeleteList(List L)
 {
-	Position P,Tmp;
-	P = L->Next;/*Header assumed*/
+	Position P,tmp;
+	
+	P = L->Next;
 	L->Next= NULL;
 	while(P != NULL)
 	{
-		Tmp = P->Next;
+		tmp = P->Next;
 		free(P);
-		P = Tmp;
+		P = tmp;
 	}
 }
 
-
+/* 
+Position Header(List L) 
+参数 
+L 要查找的链表 
+返回值 
+返回链表L的头节点位置 
+功能 
+返回头节点 
+*/ 
 Position Header(List L)
 {
 	return L;
 }
-
+/* 
+Position First(List L) 
+参数 
+L 要查找的链表 
+返回值 
+若链表非空则返回第一个数据节点，否则返回NULL 
+功能 
+返回第一个数据节点位置 
+*/ 
 Position First(List L)
 {
 	return L->Next;
 }
-
+/* 
+Position Advance(Position P) 
+参数 
+P 当前节点位置 
+返回值 
+若P位置后继节点存在则返回其位置，否则返回NULL 
+功能 
+获得位置P后继节点位置 
+*/  
 Position Advance(Position P)
 {
-	Position ReturnP;
-	if (P == NULL)
-	{
-		ReturnP = NULL;
-	}
+	Position ret;
+	
+	if (P != NULL)
+		ret = P->Next;
 	else
-	{
-		ReturnP = P->Next;
-	}
-	return ReturnP;
+		ret = NULL;
+	
+	return ret;
 }
+/* 
+Item Retrieve(Position P) 
+参数 
+P 当前节点位置 
+返回值 
+若P非空则返回其数据项的值 
+功能 
+返回P位置的数据项 
+*/ 
 ElementType Retrieve(Position P)
 {
-	return P->Element;
+	if (P != NULL)
+		return P->Element;
+	else
+		FatalError("P is null");
 }
--- src/list.h
+++ src/list.h
@@ -1,29 +1,78 @@
 #ifndef __LIST_H
 #define __LIST_H
 
-typedef int ElementType;
 struct Node;
-typedef struct Node *PtrToNode;
-typedef PtrToNode List;
-typedef PtrToNode Position;
+typedef int 			ElementType;	/*定义数据项类型*/  
+typedef struct Node 	*PtrToNode;		/*定义节点指针*/
+typedef PtrToNode 		List;			/*定义链表指针*/
+typedef PtrToNode 		Position;		/*定义节点位置指针*/
 
-List MakeEmpty(List L);
+/* 
+功能 
+生成空链表L 
+*/
+List MakeEmpty(List L);		
+/* 
+功能 
+判定链表是否为空 
+*/ 		
 int IsEmpty(List L);
+/* 
+功能 
+判定位置P的节点是否为尾节点 
+*/  
 int IsLast(Position P, List L);
-Position Find(ElementType X,List L);
-void Delete(ElementType X,List L);
-Position FindPrevious(ElementType X,List L);
+/* 
+功能 
+在链表L中查找数据项为E的第一个节点 
+*/  
+Position Find(ElementType E,List L);
+/* 
+功能 
+在链表L中删除数据项为E的第一个节点 
+*/  
+void Delete(ElementType E,List L);
+/* 
+功能 
+在链表L中查找数据项为E的第一个节点的前驱位置 
+*/ 
+Position FindPrevious(ElementType E,List L);
+/* 
+功能 
+删除链表L所有节点 
+*/  
 void DeleteList(List L);
+/* 
+功能 
+获得链表L中头节点位置 
+*/ 
 Position Header(List L);
+/* 
+功能 
+获得链表L中第一个数据节点的位置 
+*/  
 Position First(List L);
+/* 
+功能 
+获得P位置的后继节点位置 
+*/ 
 Position Advance(Position P);
+/* 
+功能 
+获得P位置节点的数据项 
+*/
 ElementType Retrieve(Position P);
-List Insert(ElementType X,List L, Position P);
+/* 
+功能 
+在链表L中P位置插入数据项为E的节点 
+*/
+List Insert(ElementType E,List L, Position P);
 
-
-struct Node{
-        ElementType Element;
-        Position Next;
+/*节点的定义*/  
+struct Node
+{
+    ElementType Element;	/*数据域*/
+    Position Next;			/*链域*/ 
 };
 
 
